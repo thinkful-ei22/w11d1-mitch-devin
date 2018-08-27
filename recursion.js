e70aba383eb898bae0f47c4eb57b6a8e5e3ef197
dghero <devingreyhero@gmail.com>
2018-08-27 07:52:17 -0700

    init commit

---
+++ recursion.js
@@ -0,0 +1,48 @@
+/* recurions.js
+ * Created: 8/27/2018
+ * Authors: Devin Hero, Mitch Gianoni
+ */
+
+//1. Counting sheep
+
+
+
+//2. Array Double
+
+
+
+//3. Reverse String
+
+
+
+//4. nth Triangle Number
+
+
+
+//5. String Splitter
+
+
+
+//6. Binary Representation
+
+
+
+//7. Factorial
+
+
+
+//8. Fibonacci
+
+
+
+//9. Anagrams
+
+
+
+//10. Animal Hierarchy
+
+
+
+//11. Organizational Chart
+
+
