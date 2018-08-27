b8e9dcc13e29d519b786c6195a45a57df22a3e53
dghero <devingreyhero@gmail.com>
2018-08-27 08:40:40 -0700

    Complete counting sheep, array doubles, reverse string

--- recursion.js
+++ recursion.js
@@ -8,19 +8,56 @@ function logProblem(name){
   console.log('Begin', name);
 }
 
+// //Initial calls
+let res;
+
+logProblem('Counting Sheep');
+sheep(5);
+
+logProblem('Array Doubles');
+const array = [2, 4, 5];
+res = arrayDouble(array);
+console.log(res);
+
+logProblem('Reverse String');
+res = reverseString('hello squirrels');
+console.log(res);
+
 
 //1. Counting sheep
-logProblem('Counting Sheep');
+
+function sheep(num){
+  if(num === 0)
+    return 0;
+
+  console.log(`${num} - Another sheep jump over the fence`);
+  sheep(num - 1);
+  return 0;
+}
 
 
 
 //2. Array Double
 
+function arrayDouble(arr){
+  if(arr.length === 0){
+    return [];
+  }
+
+  const lastElem = arr.pop(arr.length-1) * 2;
+  return [...arrayDouble(arr), lastElem];
+}
 
 
 
 //3. Reverse String
+function reverseString(string){
+  if(string.length === 0){
+    return '';
+  }
 
+  return reverseString(string.slice(1)) + string[0];
+}
 
 
 
